55a821ca000946953f25789ec6fa5cf4009f143b
Joe simpson <joe@egmrc.com>
2024-12-10 10:45:40 -0700

    Initial Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,137 @@
-import Image from "next/image";
+"use client"
+import React, {useEffect, useRef, useState} from "react";
+import HTMLFlipBook from "react-pageflip";
+import {RenderTask} from "pdfjs-dist";
+
+// eslint-disable-next-line react/display-name
+const Page = React.forwardRef(({currPage}: { currPage: number }, ref: React.ForwardedRef<HTMLDivElement>) => {
+    const canvasRef = useRef<HTMLCanvasElement>(null);
+    const renderTaskRef = useRef<RenderTask>(null);
+
+    useEffect(() => {
+        let isCancelled = false;
+
+        async function renderPage(canvas:  HTMLCanvasElement) {
+            // Import pdfjs-dist dynamically for client-side rendering.
+            // @ts-expect-error: TypeScript cannot verify dynamic import for pdfjs-dist.
+            const pdfJS = await import('pdfjs-dist/build/pdf');
+
+            // Set up the worker.
+            pdfJS.GlobalWorkerOptions.workerSrc =
+                window.location.origin + '/pdf.worker.min.mjs';
+
+            // Load the PDF document.
+            const pdf = await pdfJS.getDocument('test.pdf').promise;
+
+            // Get the first page.
+            const page = await pdf.getPage(currPage);
+            const viewport = page.getViewport({scale: 1.5});
+
+            // Prepare the canvas.
+            const canvas1 = canvas;
+            const canvasContext = canvas1.getContext('2d');
+            canvas1.height = viewport.height;
+            canvas1.width = viewport.width;
+
+            // Ensure no other render tasks are running.
+            if (renderTaskRef.current) {
+                await renderTaskRef.current.promise;
+            }
+
+            // Render the page into the canvas.
+            const renderContext = {canvasContext, viewport};
+            const renderTask = page.render(renderContext);
+
+            // Store the render task.
+            renderTaskRef.current = renderTask;
+
+            // Wait for rendering to finish.
+            try {
+                await renderTask.promise;
+            } catch (error) {
+                // @ts-expect-error Don't need to know the error type
+                if (error.name === 'RenderingCancelledException') {
+                    console.log('Rendering cancelled.');
+                } else {
+                    console.error('Render error:', error);
+                }
+            }
+
+            if (!isCancelled) {
+                console.log('Rendering completed');
+            }
+        }
+
+        (async function () {
+            if (!canvasRef.current) return;
+            await renderPage(canvasRef.current);
+        })();
+
+        // Cleanup function to cancel the render task if the component unmounts.
+        return () => {
+            isCancelled = true;
+            if (renderTaskRef.current) {
+                renderTaskRef.current.cancel();
+            }
+        };
+    }, [currPage]);
+
+    return (
+        <div ref={ref}>
+            <canvas ref={canvasRef}/>
+        </div>
+
+    );
+});
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    const [maxPage, setMaxPage] = useState(1);
+/*    const [width, setWidth] = useState(0);
+    const [height, setHeight] = useState(0);*/
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    const renderTaskRef = useRef<RenderTask>(null);
+    const book = useRef(null);
+
+    useEffect(() => {
+        let renderTask = null;
+        if(renderTaskRef.current){
+            renderTask = renderTaskRef.current;
+        }
+        (async function () {
+            // Import pdfjs-dist dynamically for client-side rendering.
+            // @ts-expect-error: TypeScript cannot verify dynamic import for pdfjs-dist.
+            const pdfJS = await import('pdfjs-dist/build/pdf');
+
+            // Set up the worker.
+            pdfJS.GlobalWorkerOptions.workerSrc =
+                window.location.origin + '/pdf.worker.min.mjs';
+
+            // Load the PDF document.
+            const pdf = await pdfJS.getDocument('test.pdf').promise;
+            setMaxPage(pdf.numPages);
+        })();
+
+        // Cleanup function to cancel the render task if the component unmounts.
+        return () => {
+            if (renderTask) {
+                renderTask.cancel();
+            }
+        };
+    }, []);
+
+
+
+    return <>
+{/*        <button onClick={() =>
+            book.current.pageFlip().flipNext()}>Next page
+        </button>*/}
+
+        {/* @ts-expect-error Ignore required attributes since they're not really required*/}
+        <HTMLFlipBook ref={book} showCover={true} height={1100} width={950}>
+            {Array.from({length: maxPage}).map((_, index) => (
+                <Page key={index} currPage={index + 1}/>
+            ))}
+        </HTMLFlipBook>
+
+    </>;
 }
